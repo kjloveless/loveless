e4d8908670237d1e26af1c2c250035ae654c4568
Kyle Loveless <kjloveless321@gmail.com>
2024-02-01 15:16:32 -0600

    Cleanup audio playing code.

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,11 +6,15 @@ export default function Home() {
   return (
     <>
       <p>Welcome to my site. Look around!</p>
-      <div data-video="RW34u1KxGC4" data-autoplay="1" data-loop="1" id="youtube-audio">
-
+      <div
+        data-video="RW34u1KxGC4"
+        data-autoplay="1"
+        data-loop="1"
+        id="youtube-audio"
+      >
       </div>
       <script src="https://www.youtube.com/iframe_api"></script>
-      <script src="https://cdn.rawgit.com/labnol/files/master/yt.js"></script>
+      <script src="/yt.js"></script>
     </>
   );
 }
---
+++ static/yt.js
@@ -0,0 +1,41 @@
+function onYouTubeIframeAPIReady(){
+  const ctrlq = document.getElementById("youtube-audio");
+
+  const icon=document.createElement("img");
+  icon.setAttribute("id","youtube-icon");
+  icon.style.cssText="cursor:pointer;cursor:hand";
+  ctrlq.appendChild(icon);
+
+  const div = document.createElement("div");
+  div.setAttribute("id","youtube-player"),
+  ctrlq.appendChild(div);
+  
+  const toggleButton = function(play){
+    icon.setAttribute("src","/logo.svg")
+  };
+
+  ctrlq.onclick=function(){
+    player.getPlayerState()===YT.PlayerState.PLAYING||player.getPlayerState()===YT.PlayerState.BUFFERING
+      ?(player.pauseVideo(),toggleButton(!1)):(player.playVideo(),toggleButton(!0))
+  };
+
+  const player=new YT.Player("youtube-player",{
+    height:"0",
+    width:"0",
+    videoId:ctrlq.dataset.video,
+    playerVars:{
+      autoplay:ctrlq.dataset.autoplay,
+      loop:ctrlq.dataset.loop
+    },
+    events:{
+      onReady:function(e){
+        player.setPlaybackQuality("small"),
+        toggleButton(player.getPlayerState()!==YT.PlayerState.CUED)
+      },
+      onStateChange:function(e){
+        ctrlq.data===YT.PlayerState.ENDED&&o(!1)
+      }
+    }
+  })
+  
+}
