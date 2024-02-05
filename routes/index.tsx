e1734757f2ba9268e8ec6d1141433b72e5fb3188
Kyle Loveless <kjloveless321@gmail.com>
2024-02-05 10:55:15 -0600

    Add joke to index.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { handler as jokeHandler } from "./api/joke.ts";
+
+interface Joke {
+  text: string;
+}
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const res = await jokeHandler(req, ctx);
+    const joke = await res.text();
+    return ctx.render(joke);
+  },
+};
+
+export default function Home({ data }: PageProps<Joke>) {
   return (
     <>
       <p>Welcome to my site. Look around!</p>
+      { data && <p class="flex items-center">{ data }</p> }
       <div
         data-video="RW34u1KxGC4"
         data-autoplay="0"
