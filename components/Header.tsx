c52ece7e7a861b13e940d6ade917a79737cea658
Kyle Loveless <kjloveless321@gmail.com>
2024-02-08 08:53:03 -0600

    Fix Header on full size browser.

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({ active }: Props) {
   ];
 
   return (
-    <div class="bg bg-[#86efac] w-full max-w-screen-lg py-6 px-8 flex flex-col md:flex-row gap-4">
+    <div class="bg-[#86efac] w-full py-6 px-8 flex flex-col md:flex-row gap-4">
       <div class="flex items-center flex-1">
         <HeartExclamation aria-hidden="true" />
         <div class="text-2xl ml-1 font-bold">
