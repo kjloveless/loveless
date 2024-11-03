1e63462be2bf663e343b9142c448cc9d18f5d2e0
Kyle Loveless <kyle@loveless.dev>
2024-11-03 00:29:49 -0500

    fix subdomain bug...???

--- islands/Link.tsx
+++ islands/Link.tsx
@@ -10,7 +10,7 @@ export default function Link(
   ) {
     const { domain, subdomain } = getDomain(globalThis.location?.hostname);
     if (subdomain) {
-      return <a href={`https://${subdomain}.${domain}`} />;
+      return <a href={`https://${subdomain}.${href}`} {...props} />;
     }
   }
 
