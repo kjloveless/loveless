1864659265c2b4231bebd3e0d3df792cd6f64e2f
Kyle Loveless <kyle@loveless.dev>
2024-11-03 10:54:02 -0600

    add about page

---
+++ routes/about.tsx
@@ -0,0 +1,41 @@
+import { TbHeartBroken } from "@preact-icons/tb";
+import Link from "../islands/Link.tsx";
+
+export default function About() {
+  return (
+    <div class="flex flex-col px-4 py-8 mx-auto">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <TbHeartBroken className={"size-16"} />
+        <h1 class="text-center text-4xl font-bold">
+          software developer. dad.
+        </h1>
+        <h4 class="text-center text-2xl font-bold">
+          i like systems and web programming. big zig and deno fan. i like
+          music.
+        </h4>
+        <h2 class="text-center text-xl font-bold">
+          i'm learning zig and golang. brushing up on c/c++.
+        </h2>
+      </div>
+      <p class="items-start text-l font-bold">
+        some things i've built:
+        <ul>
+          <li>
+            <Link href="https://github.com/kjloveless/blip">blip</Link>
+          </li>
+          <li>
+            <Link href="https://github.com/kjloveless/zish">zish</Link>
+          </li>
+          <li>
+            <Link href="https://github.com/kjloveless/ftw-cli">ftw-cli</Link>
+          </li>
+          <li>
+            <Link href="https://github.com/kjloveless/loveless">
+              loveless.dev
+            </Link>
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+}
